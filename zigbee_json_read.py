010931c7645e8ae87a9247ca3a65f10802dcaf0e
germonado <germonado@gmail.com>
2020-09-20 23:45:29 +0900

    2020/09/20/revised_read_attribute_response_from_edge

--- zigbee_json_read.py
+++ zigbee_json_read.py
@@ -6,12 +6,12 @@ import os
 d_file = './exported_json/zigbee'
 
 command_send = []
-command_receive_from_edge = []
 report_attributes_from_edge = []
 read_attribute = []
 read_attribute_response_from_edge = []
 default_response = []
 default_response_from_edge = []
+
 
 def get_file(dirpath):
 	fileList = [s for s in os.listdir(dirpath)
@@ -19,6 +19,7 @@ def get_file(dirpath):
 	fileList.sort(key=lambda s: os.path.getmtime(os.path.join(dirpath, s)), reverse=True)
 	return fileList
 
+#This function filters packets as follow zigbee protocols
 def filtering_zigbee_zcl(filelist):
 	for f in filelist:
 		print(f)
@@ -27,8 +28,10 @@ def filtering_zigbee_zcl(filelist):
 			json_read = json.load(json_file)
 			for x in range(len(json_read)):
 				if json_read[x]['_source']['layers'].get('zbee_zcl'):
+					# This if statement distinguishes command packet by Cluster-specific 0x01
 					if json_read[x]['_source']['layers']['zbee_zcl'].get('Frame Control Field: Cluster-specific (0x01)'):
 						command_send.append(json_read[x]['_source']['layers'])
+					# This elif statement distinguishes default reponse from edge by Profile-wide 0x18
 					elif json_read[x]['_source']['layers']['zbee_zcl'].get('Frame Control Field: Profile-wide (0x18)'):
                                                 # command에 대한 response
                                                 
@@ -36,18 +39,20 @@ def filtering_zigbee_zcl(filelist):
 							default_response_from_edge.append(json_read[x]['_source']['layers'])
 						elif json_read[x]['_source']['layers']['zbee_zcl']['zbee_zcl.cmd.id'] == "1":
 							read_attribute_response_from_edge.append(json_read[x]['_source']['layers'])
+
 					elif json_read[x]['_source']['layers']['zbee_zcl'].get('Frame Control Field: Profile-wide (0x00)'):
                                                 # report에 대한 response
                                                 
 						if json_read[x]['_source']['layers']['zbee_zcl']['zbee_zcl.cmd.id'] == "11":
 							default_response.append(json_read[x]['_source']['layers'])	
 					elif json_read[x]['_source']['layers']['zbee_zcl'].get('zbee_zcl.cmd.id'):
+						# This if statement distinguishes report_attribute from edge
 						if json_read[x]['_source']['layers']['zbee_zcl']['zbee_zcl.cmd.id'] == "10":
 							report_attributes_from_edge.append(json_read[x]['_source']['layers'])
+						# This elif statement distinguishes send read_attribute 
 						elif json_read[x]['_source']['layers']['zbee_zcl']['zbee_zcl.cmd.id'] == "0":
 							read_attribute.append(json_read[x]['_source']['layers'])
-						elif json_read[x]['_source']['layers']['zbee_zcl']['zbee_zcl.cmd.id'] == "1":
-							read_attribute_response_from_edge.append(json_read[x]['_source']['layers'])
+
 			json_file.close()
 
 <<<<<<< HEAD
